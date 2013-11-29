a6e7444d1af55424e21aaa7d031253e7919181af
liperosas <lipe.galoh@gmail.com>
2013-11-29 00:14:03 -0300

    Login - ok Cadastro concursando - ok

---
+++ ProjetoPscClasses/src/negocios/RNConcursando.java
@@ -0,0 +1,54 @@
+package negocios;
+
+import java.util.List;
+
+import classes.Concursando;
+import classes.Fase;
+import daoImpl.ConcursandoDAOImpl;
+import factory.FactoryDAO;
+
+public class RNConcursando extends RNPessoa {
+
+    ConcursandoDAOImpl dao;
+
+    public void inserir(Concursando concursando) throws Exception {
+        this.validarInserir(concursando);
+        dao.inserir(concursando);
+    }
+
+    public void atualizar(Concursando concursando) throws Exception {
+        this.validarAlterar(concursando);
+        dao.atualizar(concursando);
+    }
+
+    public void remover(long id) throws Exception {
+        this.validarRemover(id);
+        dao.remover(id);
+    }
+
+    public Concursando consultarPorId(long id) throws Exception {
+        return dao.consultarPorId(id);
+    }
+
+    public List<Concursando> consultarTodos() throws Exception {
+        return dao.consultarTodos();
+    }
+
+    public Concursando logarConcursando(String login, String senha)
+            throws Exception {
+        Concursando concursando = dao.logarConcursando(login, senha);
+        if (concursando == null) {
+            throw new Exception("Login ou Senha Inv�lidos");
+        }
+        return concursando;
+    }
+
+    public List<Concursando> calcularNotaMultiplaConcursandos(Fase fase) throws Exception {
+        return dao.calcularNotaMultiplaConcursandos(fase);
+    }
+
+    public RNConcursando() {
+        // TODO Auto-generated constructor stub
+        dao = FactoryDAO.getCocursandoDAOImpl();
+    }
+}
